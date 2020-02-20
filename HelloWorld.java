22f18965d1fb18138993af8e0d4f3128a09b4e1e
swasa1329 <52796476+swasa1329@users.noreply.github.com>
2020-02-20 16:42:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Welcome to Leena IT"); 
+        System.out.println("Hello World!, Welcome to Leena IT"); 
     } 
 } 
