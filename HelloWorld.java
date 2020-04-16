8272fd503cfb3d85f18c3b00d3003df1977d3289
venkata-satish-github <61977785+venkata-satish-github@users.noreply.github.com>
2020-04-16 18:56:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello World!, WELCOME TO LEENA IT"); 
+        System.out.println("Hello World!, WELCOME TO jenkins-training"); 
     } 
 } 
